51acf2596fb98b350f3389d12174c62bcc45b3e8
Samith <samithwijesinha@gmail.com>
2023-05-16 08:21:47 +0530

    adjusments to the plane detection

--- NoobARKit/ViewController.swift
+++ NoobARKit/ViewController.swift
@@ -21,12 +21,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,7 +29,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        // Enable horizontal plane detection
+        configuration.planeDetection = .horizontal
+        
+        // show Feature Points
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -48,14 +49,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
